3fab1cb575de479802f0f208d8608681b76f58a3
Steve Carter Feujo Nomeny <scnomeny@mit.edu>
2024-09-23 18:26:39 +0200

    updated to remove dependency of filesystem

--- src/pArduBridge/main.cpp
+++ src/pArduBridge/main.cpp
@@ -1,11 +1,14 @@
 /************************************************************/
-/*    NAME: Steve Carter Feujo Nomeny                                              */
-/*    ORGN: MIT, Cambridge MA                               */
-/*    FILE: main.cpp, Cambridge MA                          */
-/*    DATE: December 29th, 1963                             */
+/*    NAME: Steve Carter Feujo Nomeny                        */
+/*    ORGN: MIT, Cambridge MA                                */
+/*    FILE: main.cpp, Cambridge MA                           */
+/*    DATE: December 29th, 1963                              */
 /************************************************************/
 
 #include <string>
+#include <iostream>
+#include <unistd.h>  // For getcwd
+#include <limits.h>  // For PATH_MAX
 #include "MBUtils.h"
 #include "ColorParse.h"
 #include "ArduBridge.h"
@@ -13,61 +16,63 @@
 
 using namespace std;
 
-
 #include <log.h>
-#include <filesystem>
-
 
 int main(int argc, char *argv[])
 {
-  string mission_file;
-  string run_command = argv[0];
+    string mission_file;
+    string run_command = argv[0];
 
-  for(int i=1; i<argc; i++) {
-    string argi = argv[i];
-    if((argi=="-v") || (argi=="--version") || (argi=="-version"))
-      showReleaseInfoAndExit();
-    else if((argi=="-e") || (argi=="--example") || (argi=="-example"))
-      showExampleConfigAndExit();
-    else if((argi == "-h") || (argi == "--help") || (argi=="-help"))
-      showHelpAndExit();
-    else if((argi == "-i") || (argi == "--interface"))
-      showInterfaceAndExit();
-    else if(strEnds(argi, ".moos") || strEnds(argi, ".moos++"))
-      mission_file = argv[i];
-    else if(strBegins(argi, "--alias="))
-      run_command = argi.substr(8);
-    else if(i==2)
-      run_command = argi;
-  }
-  
-  if(mission_file == "")
-    showHelpAndExit();
+    for(int i=1; i<argc; i++) {
+        string argi = argv[i];
+        if((argi=="-v") || (argi=="--version") || (argi=="-version"))
+            showReleaseInfoAndExit();
+        else if((argi=="-e") || (argi=="--example") || (argi=="-example"))
+            showExampleConfigAndExit();
+        else if((argi == "-h") || (argi == "--help") || (argi=="-help"))
+            showHelpAndExit();
+        else if((argi == "-i") || (argi == "--interface"))
+            showInterfaceAndExit();
+        else if(strEnds(argi, ".moos") || strEnds(argi, ".moos++"))
+            mission_file = argv[i];
+        else if(strBegins(argi, "--alias="))
+            run_command = argi.substr(8);
+        else if(i==2)
+            run_command = argi;
+    }
 
-  cout << termColor("green");
-  cout << "pArduBridge launching as " << run_command << endl;
-  cout << termColor() << endl;
+    if(mission_file == "")
+        showHelpAndExit();
 
-  ArduBridge ArduBridge;
+    cout << termColor("green");
+    cout << "pArduBridge launching as " << run_command << endl;
+    cout << termColor() << endl;
 
-  // Retrieve the current working directory
-  std::filesystem::path current_path = std::filesystem::current_path();
-  
-  // Construct the full path for the log file
-  std::filesystem::path log_file_path = current_path / "output.log";
+    ArduBridge ArduBridge;
 
-  // Set the log file with the full path
-  mavsdk::set_log_file(log_file_path.string());
+    // Retrieve the current working directory using getcwd
+    char current_path[PATH_MAX];
+    if (getcwd(current_path, sizeof(current_path)) != NULL) {
+        std::string current_path_str(current_path);
 
-  // Example log output
-  mavsdk::get_log_stream() << "This is a test log entry." << std::endl;
-  std::cout << "This is a test log entry." << std::endl;
+        // Construct the full path for the log file
+        std::string log_file_path = current_path_str + "/output.log";
 
-  // Ensure the output is flushed to the file
-  // mavsdk::get_log_stream().flush();
+        // Set the log file with the full path
+        mavsdk::set_log_file(log_file_path);
 
-  ArduBridge.Run(run_command.c_str(), mission_file.c_str());
-  
-  return(0);
+        // Example log output
+        mavsdk::get_log_stream() << "This is a test log entry." << std::endl;
+        std::cout << "This is a test log entry." << std::endl;
+
+        // Ensure the output is flushed to the file
+        // mavsdk::get_log_stream().flush();
+    } else {
+        std::cerr << "Error retrieving current working directory" << std::endl;
+        return 1;
+    }
+
+    ArduBridge.Run(run_command.c_str(), mission_file.c_str());
+
+    return(0);
 }
-
