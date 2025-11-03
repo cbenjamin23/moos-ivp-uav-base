8ee96a4e7c0a1ddd1d5c0c7bbd28ed5a45246141
Charlie Benjamin <chbenj36@gmail.com>
2025-11-03 15:41:58 -0500

    actually meant to add this version of the alpha ufld mission since the last one didn't have correct launch scripts

--- missions/alpha_ufld/clean.sh
+++ missions/alpha_ufld/clean.sh
@@ -20,18 +20,30 @@ for ARGI; do
     elif [ "${ARGI}" = "--verbose" -o "${ARGI}" = "-v" ] ; then
 	VERBOSE="-v"
     else 
-	echo "clean.sh: Bad Arg:" $ARGI
+	echo "clean.sh: Bad Arg:[$ARGI]. Exit Code 1."
 	exit 1
     fi
 done
 
 #-------------------------------------------------------
-#  Part 2: Do the cleaning!
+#  Part 3: Do the cleaning!
 #-------------------------------------------------------
 if [ "${VERBOSE}" = "-v" ]; then
     echo "Cleaning: $PWD"
 fi
 rm -rf  $VERBOSE   MOOSLog_*  XLOG_* LOG_* 
-rm -f   $VERBOSE   *~  *.moos++
-rm -f   $VERBOSE   targ_*
+rm -f   $VERBOSE   *~  *.moos++ results.log
+rm -f   $VERBOSE   targ_* tmp_* *.tar
 rm -f   $VERBOSE   .LastOpenedMOOSLogDirectory
+rm -f   $VERBOSE   .mem_info* vloiterpos.txt vpositions.txt vdests.txt
+rm -f   $VERBOSE   vnames.txt vcolors.txt vcolors.txt vspeeds.txt
+rm -f   $VERBOSE   *.moosx *.bhvx
+
+for file in *; do
+    if [ -d $file ]; then
+	ls $file/*.alog >& /dev/null
+	if [ $? = 0 ]; then
+	    rm -rf $file
+	fi
+    fi
+done
